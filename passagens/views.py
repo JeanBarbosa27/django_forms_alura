a5236b775f9cf8a48bcb86357b91c71b3b69eb43
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-12-11 07:26:43 -0300

    Cria formulário e página de revisão de dados

--- passagens/views.py
+++ passagens/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-
+from passagens.forms import PassagemForm
 
 def index(request):
-    return render(request, 'index.html')
+    form = PassagemForm()
+
+    contexto = { 'form': form }
+
+    return render(request, 'index.html', contexto)
+
+
+def revisao_consulta(request):
+    if request.method == 'POST':
+        form = PassagemForm(request.POST)
+        contexto = { 'form': form }
+
+        return render(request, 'minha_consulta.html', contexto)
