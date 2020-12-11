01fb3f244ccfd029f62159e4235c2070141c2893
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-12-11 08:00:58 -0300

    Adiciona mais campos ao formulário

--- passagens/forms.py
+++ passagens/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 from tempus_dominus.widgets import DatePicker
+from datetime import datetime
+from .classe_viagem import tipos_de_classe
 
 
 class PassagemForm(forms.Form):
-    origem = forms.CharField(label='Origem', max_length=100)
-    destino = forms.CharField(label='Destino', max_length=100)
-    data_ida = forms.DateField(label="Ida", widget=DatePicker())
-    data_volta = forms.DateField(label="Volta", widget=DatePicker())
+    origem = forms.CharField(label='Origem:', max_length=100)
+    destino = forms.CharField(label='Destino:', max_length=100)
+    data_ida = forms.DateField(label='Ida:', widget=DatePicker())
+    data_volta = forms.DateField(label='Volta:', widget=DatePicker())
+    data_pesquisa = forms.DateField(label='Pesquisado em:', initial=datetime.today, disabled=True)
+    classe_viagem = forms.ChoiceField(label='Classe de vôo:', choices=tipos_de_classe)
+    informacoes = forms.CharField(label='Informações extras:', required=False, widget=forms.Textarea())
+    email = forms.EmailField(label='Email:', max_length=150)
